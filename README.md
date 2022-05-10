6418e5bcab7afc6d5278357a83d1b62f3d2ccaf6
atk <atkwaves@gmail.com>
2022-05-10 20:40:57 -0300

    A00 Projeto inicial

---
+++ README.md
@@ -0,0 +1,19 @@
+## Curso de Java Servlet: Autenticação, Autorização e o Padrão MVC
+
+### Conhecimento:
+
+- Desenvolva um sistema com autenticação e autorização
+- Use o objeto HttpSession e aprenda sobre Cookies
+- Construa as camadas MVC baseados em Filters e Servlets
+- Faça deploy no servidor Jetty
+- Implemente o seu próprio WebService com JSON e XML
+
+### Aulas:
+
+1. Criando o controlador
+2. O padrão MVC
+3. Formulário de login
+4. HttpSession
+5. Aplicando filtros
+6. Introdução ao Web Services
+7. Deploy no Jetty
---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,30 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet(urlPatterns = "/oi")
+public class OiMundoServlet extends HttpServlet {
+
+	public OiMundoServlet() {
+		System.out.println("Criando Oi Mundo Servlet");
+	}
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Oi mundo, parabéns você escreveu o primeiro servlets.");
+		out.println("</body>");
+		out.println("</html>");
+
+		System.out.println("O servlet OiMundoServlet foi chamado!");
+	}
+}
