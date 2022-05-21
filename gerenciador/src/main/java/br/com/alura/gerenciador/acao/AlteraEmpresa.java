98f12cea7fea11bdaf2d52b79d20a33ea566c952
atk <atkwaves@gmail.com>
2022-05-21 18:51:15 -0300

    A01 Criando o controlador

--- gerenciador/src/main/java/br/com/alura/gerenciador/acao/AlteraEmpresa.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/acao/AlteraEmpresa.java
@@ -1,4 +1,4 @@
-package br.com.alura.gerenciador.servlet;
+package br.com.alura.gerenciador.acao;
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -6,24 +6,22 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/alteraEmpresa")
-public class AlteraEmpresaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+public class AlteraEmpresa {
 
-		System.out.println("Alterando empresa!");
+	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String nomeEmpresa = request.getParameter("nome");
 		String paramDataEmpresa = request.getParameter("data");
 		String paramId = request.getParameter("id");
 		Integer id = Integer.valueOf(paramId);
+
+		System.out.println("Ação Altera Empresa " + id);
 
 		Date dataAbertura = null;
 
@@ -34,14 +32,11 @@ public class AlteraEmpresaServlet extends HttpServlet {
 			throw new ServletException(e);
 		}
 
-		System.out.println(id);
-
 		Banco banco = new Banco();
 		Empresa empresa = banco.buscaEmpresaPelaId(id);
 		empresa.setNome(nomeEmpresa);
 		empresa.setDataAbertura(dataAbertura);
 
-		response.sendRedirect("listaEmpresas");
+		response.sendRedirect("entrada?acao=ListaEmpresas");
 	}
-
 }
--- gerenciador/src/main/java/br/com/alura/gerenciador/acao/NovaEmpresa.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/acao/NovaEmpresa.java
@@ -1,27 +1,20 @@
-package br.com.alura.gerenciador.servlet;
+package br.com.alura.gerenciador.acao;
 
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class NovaEmpresaServlet
- */
-@WebServlet("/novaEmpresa")
-public class NovaEmpresaServlet extends HttpServlet {
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
 
-	private static final long serialVersionUID = 1L;
+public class NovaEmpresa {
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public void executa(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
 		System.out.println("Cadastrando nova empresa!");
 
@@ -46,12 +39,6 @@ public class NovaEmpresaServlet extends HttpServlet {
 
 		request.setAttribute("empresa", empresa.getNome());
 
-		response.sendRedirect("listaEmpresas");
-
-//		Chamar o JSP
-//		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas");
-//		request.setAttribute("empresa", empresa.getNome());
-//		rd.forward(request, response);
+		response.sendRedirect("entrada?acao=ListaEmpresas");
 	}
-
 }
