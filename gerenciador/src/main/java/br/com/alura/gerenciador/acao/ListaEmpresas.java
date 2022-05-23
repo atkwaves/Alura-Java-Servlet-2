35514a486cec482b8510beef8df56905e8ec58e1
atk <atkwaves@gmail.com>
2022-05-23 20:48:06 -0300

    A04 HttpSession

--- gerenciador/src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
@@ -15,11 +16,16 @@ public class ListaEmpresas implements Acao {
 	public String executa(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+//		HttpSession sessao = request.getSession();
+//		if (sessao.getAttribute("usuarioLogado") == null) {
+//			return "redirect:entrada?acao=LoginForm";
+//		}
+
 		System.out.println("Listando Empresas");
 
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
-		
+
 		request.setAttribute("empresas", lista);
 
 		return "forward:listaEmpresas.jsp";
